97671a2a64705e4c7dc6764d96572afc60c3840f
Carlos Soria <soriac367@gmail.com>
2023-01-24 14:44:17 -0500

    Tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""async python"""
+
+import asyncio
+from typing import List, Awaitable
+import time
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Awaitable:
+    """Task factory"""
+    return asyncio.create_task(wait_random(max_delay))
