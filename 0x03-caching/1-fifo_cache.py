96327958a6b2db4f3fc3a0b690636e9accc27f76
Carlos Soria <soriac367@gmail.com>
2023-01-27 20:45:23 -0500

    FIFO cache

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Caching in python"""
+from dis import dis
+import base_caching
+import collections
+
+
+class FIFOCache(base_caching.BaseCaching):
+    """Defines a FIFO cache and its methods"""
+
+    def __init__(self):
+        super().__init__()
+        self.key_queue = collections.deque()
+    
+    def put(self, key, item):
+        """Inserts or updates a cache"""
+        if key and item:
+            self.cache_data.update({key: item})
+            self.key_queue.append(key)
+            if len(self.cache_data) > 4:
+                discard = self.key_queue.popleft()
+                self.cache_data.pop(discard)
+                print(f"DISCARD {discard}")
+
+    def get(self, key):
+        """Retrieves from the cache"""
+        return self.cache_data.get(key, None)
