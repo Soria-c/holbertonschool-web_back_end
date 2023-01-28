4b50c4ccff9bd309a0e3d778c662268ce75ccb3e
Carlos Soria <soriac367@gmail.com>
2023-01-27 20:48:50 -0500

    FIFO cache

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -10,13 +10,13 @@ class FIFOCache(base_caching.BaseCaching):
     def __init__(self):
         super().__init__()
         self.key_queue = collections.deque()
-    
+
     def put(self, key, item):
         """Inserts or updates a cache"""
         if key and item:
             self.cache_data.update({key: item})
             self.key_queue.append(key)
-            if len(self.cache_data) > super.MAX_ITEMS:
+            if len(self.cache_data) > super().MAX_ITEMS:
                 discard = self.key_queue.popleft()
                 self.cache_data.pop(discard)
                 print(f"DISCARD {discard}")
