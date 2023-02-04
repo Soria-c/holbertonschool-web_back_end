517e160ffcede8f880d581afb599b0867974b97a
Carlos Soria <soriac367@gmail.com>
2023-02-04 00:25:34 -0500

    Filtered logger 2

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """obfuscator"""
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -11,3 +12,21 @@ def filter_datum(fields: List[str], redaction: str,
     for i in fields:
         os = re.sub(rf"{i}=(.+?)(?={separator})", f"{i}={redaction}", os)
     return os
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """returns formated string"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
