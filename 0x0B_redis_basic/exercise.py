12c176c2fa412dc6935d88fc0c1a0e4d2d64082d
Carlos Soria <soriac367@gmail.com>
2023-03-07 22:34:44 -0500

    ff

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,35 +1,148 @@
 #!/usr/bin/env python3
-"""Python Redis"""
+"""
+Definition of Cache Class
+"""
 
-from redis import Redis
 import uuid
-from typing import Union, Callable, Optional
+from functools import wraps
+from typing import Callable, List, Optional, Union
+
+import redis
+
+
+def replay(method: Callable):
+    """
+    Shows the times that a method was called
+    as a replay
+    Args:
+        method (Callable): Method to be replaied
+    """
+    db: redis.Redis = redis.Redis()
+    key: str = method.__qualname__
+
+    input_key: str = "{}:inputs".format(key)
+    output_key: str = "{}:outputs".format(key)
+
+    inputs: List[bytes] = db.lrange(input_key, 0, -1)
+    outputs: List[bytes] = db.lrange(output_key, 0, -1)
+
+    count: str = db.get(key).decode('utf-8')
+
+    print("{} was called {} times:".format(key, count))
+
+    for input, output in zip(inputs, outputs):
+        input = input.decode('utf-8')
+        output = output.decode('utf-8')
+        print("{}(*{}) -> {}".format(key, input, output))
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator that saves the call history of a given method
+    saves on redis, a list of a input and output of the method
+    Args:
+        method (Callable): Method to be decorated
+    Returns:
+        Callable: Wrapped Method
+    """
+    key: str = method.__qualname__
+    input_key: str = "{}:inputs".format(key)
+    output_key: str = "{}:outputs".format(key)
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper that saves the history on redis
+        inputs and outputs
+        """
+        self._redis.rpush(input_key, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, res)
+        return res
+
+    return wrapper
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator to count the times that
+    a method is called
+    """
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Logig of the decorator that counts
+        the times that a method is called
+        """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
-    """Redis class"""
+    """
+    Cache class
+    """
+
     def __init__(self):
-        """Constructor"""
-        self._redis: Redis = Redis()
+        """
+        Constructor of Cache class
+        """
+        self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Sets a key value pair"""
+        """
+        Saves a data with a random key
+        Args:
+            data (Union[str, bytes, int, float]): Data
+            to save on Redis
+        Returns:
+            str: Key of the data
+        """
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, int]:
-        """Retrieves data saved in redis given a key"""
-        value = self._redis.get(key)
-        if fn:
-            value = fn(value)
-        return value
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """
+        Get the data saved on Redis, with a given key
+        Args:
+            key (str): Key of the data to be retrieved
+            fn (Optional[Callable]): Callback
+            function to convert the type of the data
+        """
+        data = self._redis.get(key)
+        if (fn):
+            return fn(data)
+        return data
 
-    def get_str(self, key: str) -> str:
-        """Retrieves data and casts it to string"""
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Get the data saved on Redis as a string,
+        with the given key
+        Args:
+            key (str): Key to search
+            the value on Redis
+        Returns:
+            Optional[str]: Return the value of
+            the key as a string
+        """
         return self.get(key, str)
 
-    def get_str(self, key: int) -> int:
-        """Retrieves data and casts it to int"""
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Get the data saved on Redis as an int,
+        with the given key
+        Args:
+            key (str): Key to search
+            the value on Redis
+        Returns:
+            Optional[int]: Return the value of
+            the key as a int
+        """
         return self.get(key, int)
---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Implementing an expiring web cache and tracker
+"""
+
+from functools import wraps
+from typing import Callable, Optional
+
+import redis
+import requests
+
+db: redis.Redis = redis.Redis()
+
+
+def count_times_requested_url(func: Callable) -> Callable:
+    """
+    Count the times that an url was requested,
+    and save on cache the HTML Content
+    Args:
+        func (Callable): Function to be decorated
+    Returns:
+        Callable: Wrapper
+    """
+
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        """
+        Logic of the decorator
+        Args:
+            url (str): URL to the page
+        Returns:
+            str: HTML content of the page
+        """
+        cached_url: str = "saved:{}".format(url)
+        cached_reponse: Optional[bytes] = db.get(cached_url)
+
+        if cached_reponse:
+            return cached_reponse.decode("utf-8")
+
+        url_counter_key: str = "count:{}".format(url)
+        response = func(url)
+
+        db.incr(url_counter_key)
+        db.set(cached_url, response)
+        db.expire(cached_url, 10)
+        return response
+
+    return wrapper
+
+
+@count_times_requested_url
+def get_page(url: str) -> str:
+    """
+    Retrieve the HTML content of a particular UR.
+    Args:
+        url (str): URL to get the page
+    Returns:
+        str: HTML content of the URL
+    """
+    return requests.get(url).text
