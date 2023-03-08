6aad27c41df816ce5d27bd052c19a01022609d36
Carlos Soria <soriac367@gmail.com>
2023-03-07 20:19:39 -0500

    Exercise

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,17 +3,18 @@
 
 from redis import Redis
 import uuid
+from typing import Union
 
 
 class Cache():
     """Redis class"""
     def __init__(self):
         """Constructor"""
-        self._redis = Redis()
+        self._redis: Redis = Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Sets a key value pair"""
-        key = str(uuid.uuid1())
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
