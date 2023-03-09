7716e6507eb7ca85ec7319ba50c2939ec624857f
Carlos Soria <soriac367@gmail.com>
2023-03-09 12:24:18 -0500

    script that creates a stored procedure AddBonus that adds a new correction for a studen

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,19 @@
+-- script that creates a stored procedure AddBonus that adds a new correction for a studen
+
+-- defining the procedure
+DELIMITER $$
+CREATE PROCEDURE AddBonus(
+    IN user_id INT ,
+    project_name VARCHAR(255),
+    score FLOAT
+)
+BEGIN
+    DECLARE project_id INT;
+    SELECT id INTO project_id FROM projects WHERE name=project_name;
+    IF project_id IS NULL
+        THEN INSERT INTO projects (name) VALUES(project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, project_id, score);
+END$$
