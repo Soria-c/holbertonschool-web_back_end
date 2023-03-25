7dc76593b4cdb97551c770e8493402a09681bc9a
Carlos Soria <soriac367@gmail.com>
2023-03-25 11:56:37 -0500

    Script that provides some stats about Nginx logs stored in MongoDB

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from pymongo import MongoClient
 """Script that provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
 
 if __name__ == "__main__":
     with MongoClient() as client:
