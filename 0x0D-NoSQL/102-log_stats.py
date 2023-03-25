ec00879614a1d37e278229528c89802e1b60b2cc
Carlos Soria <soriac367@gmail.com>
2023-03-25 13:24:10 -0500

    Logs v2

--- 0x0D-NoSQL/102-log_stats.py
+++ 0x0D-NoSQL/102-log_stats.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 """Script that provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
-from pymongo.collection import Collection
-from pymongo.database import Database
 
 
 if __name__ == "__main__":
     with MongoClient() as client:
         """Mongodb Client"""
         db = client.logs
-        collection: Collection = db.nginx
+        collection = db.nginx
         print("{:d} logs".format(collection.count_documents({})))
         print("Methods:")
         for m in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
