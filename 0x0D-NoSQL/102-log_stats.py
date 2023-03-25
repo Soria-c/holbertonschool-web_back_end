f273744c5e8598886b1370ae2ab8c565531de3d7
Carlos Soria <soriac367@gmail.com>
2023-03-25 13:20:55 -0500

    Logs v2

---
+++ 0x0D-NoSQL/102-log_stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Script that provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from pymongo.database import Database
+
+
+if __name__ == "__main__":
+    with MongoClient() as client:
+        """Mongodb Client"""
+        db = client.logs
+        collection: Collection = db.nginx
+        print("{:d} logs".format(collection.count_documents({})))
+        print("Methods:")
+        for m in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+            print("\tmethod {:s}: {:d}"
+                  .format(m, collection.count_documents({"method": m})))
+        print("{:d} status check"
+              .format(collection
+                      .count_documents({"method": "GET", "path": "/status"})))
+        ips = collection.aggregate([
+        {
+            '$group': {
+                '_id': '$ip',
+                'count': {
+                    '$sum': 1
+                }
+            }
+        },
+        {
+            '$sort': {
+                'count': -1
+            }
+        },
+        {
+            '$limit': 10
+        }
+    ])
+
+    print("IPs:")
+    for ip in ips:
+        print("\t{:s}: {:d}".format(ip.get("_id"), ip.get("count")))
