8434c58c6b688734777ee8415a1f82e1d2e26d6f
Carlos Soria <soriac367@gmail.com>
2023-03-25 11:35:34 -0500

    Script that provides some stats about Nginx logs stored in MongoDB

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 from pymongo import MongoClient
-"""Pymongo"""
+"""Script that provides some stats about Nginx logs stored in MongoDB"""
 
-
-with MongoClient() as client:
-    db = client.logs
-    collection = db.nginx
-    print("{:d} logs".format(collection.count_documents({})))
-    for m in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
-        print("\tmethod {:s}: {:d}"
-              .format(m, collection.count_documents({"method": m})))
-    print("{:d} status check"
-          .format(collection
-                  .count_documents({"method": "GET", "path": "/status"})))
+if __name__ == "__main__":
+    with MongoClient() as client:
+        db = client.logs
+        collection = db.nginx
+        print("{:d} logs".format(collection.count_documents({})))
+        for m in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+            print("\tmethod {:s}: {:d}"
+                .format(m, collection.count_documents({"method": m})))
+        print("{:d} status check"
+            .format(collection
+                    .count_documents({"method": "GET", "path": "/status"})))
