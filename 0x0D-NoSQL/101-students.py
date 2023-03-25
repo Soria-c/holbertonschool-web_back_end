1c75212fe13af16ba3a2926741e83aca60d2cb9e
Carlos Soria <soriac367@gmail.com>
2023-03-25 13:06:59 -0500

    Function that returns all students sorted by average score

---
+++ 0x0D-NoSQL/101-students.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""pymongo"""
+
+
+def top_students(mongo_collection):
+    """Function that returns all students sorted by average score """
+    return mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {
+                    "$avg": "$topics.score"
+                }
+            }
+        },
+        {
+            "$sort": {
+                "averageScore": -1
+            }
+        }
+    ])
